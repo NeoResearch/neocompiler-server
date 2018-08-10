3b82946ece1d575ace900bc25b09d82a12f63f9d
NeoCompiler Server <root-neocompilerserver@digitalocean>
2018-08-10 18:53:45 +0000

    get Certification and minor adjusts

---
+++ getCertificates.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+letsencrypt certonly --standalone --email igormachado@gmail.com -d neocompiler.io -d pyrest.neocompiler.io -d pyrpc.neocompiler.io -d eco.neocompiler.io -d neoscan.neocompiler.io -d rpc.neocompiler.io -d rest.neocompiler.io -d node1.neocompiler.io -d node2.neocompiler.io -d node3.neocompiler.io -d node4.neocompiler.io -d ecoservices.neocompiler.io -d compilers.neocompiler.io
