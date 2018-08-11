0d8ffaf9fa607d73d404ca17c31ea816f1951587
NeoCompiler Server <root-neocompilerserver@digitalocean>
2018-08-11 18:44:22 +0000

    updating crontab commands

--- updateInterfaceNeoCompilerStop.sh
+++ updateInterfaceNeoCompilerStop.sh
@@ -5,7 +5,7 @@ COMMIT1=`git log --format="%H" -n 1`
 ./pull.sh
 COMMIT2=`git log --format="%H" -n 1`
 
-[[ $COMMIT1 == $COMMIT2 ]] && (echo "no change" && date) || (echo "git changed" && date && ./npm_prune_install.sh && ./buildCompilers_startWebInterface.sh)
+[[ $COMMIT1 == $COMMIT2 ]] && (echo "no change" && date) || (echo "git changed" && date && ./npm_prune_install.sh && ./buildCompilers.sh && nohup ./runHttpExpress.sh > ./express-servers/outputs/nohupOutputRunHttpExpress.out 2> ./express-servers/outputs/nohupOutputRunHttpExpress.err < /dev/null && (cd express-servers; ./startAllExpressNohup.sh) )
 
 #./stop.sh
 #git pull
