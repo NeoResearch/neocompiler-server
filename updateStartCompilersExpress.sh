af5636be2891258f3f4acc6b05e864d4bd0c726b
neoresearch-compilers-neotests <neoresearch-compilers-neotests@neoresearch.io>
2018-08-11 14:30:47 +0000

    updating crontab

--- updateStartCompilersExpress.sh
+++ updateStartCompilersExpress.sh
@@ -4,4 +4,4 @@ COMMIT1=`git log --format="%H" -n 1`
 ./pull.sh
 COMMIT2=`git log --format="%H" -n 1`
 
-[[ $COMMIT1 == $COMMIT2 ]] && (echo "no change" && date) || (echo "git changed" && date && ./npm_prune_install.sh && (cd express-servers; nohup ./run-CompilerExpress-RPC.sh > ./outputs/nohupCompilers.out 2> ./outputs/nohupCompilers.err < /dev/null &) )
+[[ $COMMIT1 == $COMMIT2 ]] && (echo "no change" && date) || (echo "git changed" && date && ./npm_prune_install.sh && (cd express-servers; ./run-CompilerExpress-RPC.sh )
