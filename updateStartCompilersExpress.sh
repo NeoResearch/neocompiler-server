e731964a1d6483d2a82f2016c651892f85eadd0f
neoresearch-compilers-neotests <neoresearch-compilers-neotests@neoresearch.io>
2018-11-19 01:49:06 +0000

    Minnor modification on update compilers

--- updateStartCompilersExpress.sh
+++ updateStartCompilersExpress.sh
@@ -4,4 +4,4 @@ COMMIT1=`git log --format="%H" -n 1`
 ./pull.sh
 COMMIT2=`git log --format="%H" -n 1`
 
-[[ $COMMIT1 == $COMMIT2 ]] && (echo "no change" && date) || (echo "git changed" && date && ./npm_prune_install.sh && (cd compilers/docker-compiler-csharp; ./docker_build_list_of_compilers.sh) &&(cd express-servers; ./run-CompilerExpress-RPC.sh) )
+[[ $COMMIT1 == $COMMIT2 ]] && (echo "no change" && date) || (echo "git changed" && date && ./npm_prune_install.sh && ./buildCompilers.sh && (cd compilers/docker-compiler-csharp; ./docker_build_list_of_compilers.sh) && (cd express-servers; ./run-CompilerExpress-RPC.sh) )
