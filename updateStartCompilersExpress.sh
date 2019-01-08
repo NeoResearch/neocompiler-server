1b5eb5a77e21496a50fe96a630a3530f7d45a602
neoresearch-compilers-neotests <neoresearch-compilers-neotests@neoresearch.io>
2019-01-08 14:12:24 +0000

    Updating pull on compilers server

--- updateStartCompilersExpress.sh
+++ updateStartCompilersExpress.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd neocompiler-eco
 COMMIT1=`git log --format="%H" -n 1`
-./pull.sh
+git pull
 COMMIT2=`git log --format="%H" -n 1`
 
 [[ $COMMIT1 == $COMMIT2 ]] && (echo "no change" && date) || (echo "git changed" && date && ./npm_prune_install.sh && ./buildCompilers.sh && (cd compilers/docker-compiler-csharp; ./docker_build_list_of_compilers.sh) && (cd express-servers; ./run-CompilerExpress-RPC.sh) )
