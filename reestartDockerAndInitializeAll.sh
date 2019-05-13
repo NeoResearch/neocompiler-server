c37ff7dab80faf5247449454f1abbbae979384d0
NeoCompiler Server <root-neocompilerserver@digitalocean>
2019-05-13 13:21:25 +0000

    Improving restartscript

--- reestartDockerAndInitializeAll.sh
+++ reestartDockerAndInitializeAll.sh
@@ -1,7 +1,8 @@
-echo "Reestarting service docker..."
+echo "\n (RESTART) Reestarting service docker..."
 service docker restart
-echo "Building, composing and up on nginx..."
+echo "\n (RESTART) All dockers have been killed!"
+echo "\n (RESTART) NGIX Building, down and up dettached"
 docker-compose build && docker-compose down && docker-compose up -d
-echo "updating all Neo Compiler Eco..."
+echo "\n (RESTART) calling standard ./updateAllNeoCompiler script..."
 ./updateAllNeoCompiler.sh
-echo "Done!"
+echo "\n (RESTART) Done! :)"
