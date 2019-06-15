39b8ffe7d29e7ddfea1605de035f0c4b7ef5e9c1
NeoCompiler Server <root-neocompilerserver@digitalocean>
2019-06-15 22:37:11 +0000

    fixing reestart

--- reestartDockerAndInitializeAll.sh
+++ reestartDockerAndInitializeAll.sh
@@ -1,8 +1,16 @@
+#!/bin/sh
+
 echo "\n (RESTART) Reestarting service docker..."
 service docker restart
+
+echo "\n (RESTART) Docker prune network..."
+docker network prune -f
+
 echo "\n (RESTART) All dockers have been killed!"
 echo "\n (RESTART) NGIX Building, down and up dettached"
 docker-compose build && docker-compose down && docker-compose up -d
+
 echo "\n (RESTART) calling standard ./updateAllNeoCompiler script..."
 ./updateAllNeoCompiler.sh
+
 echo "\n (RESTART) Done! :)"
