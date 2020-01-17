8e7172a1ccf07f591f3665a07d5450b357d3cf63
Vitor <vncoelho@gmail.com>
2020-01-17 19:25:38 -0300

    Update metadata

--- updateMetadata.sh
+++ updateMetadata.sh
@@ -1,5 +1,5 @@
 cd ./metadata
-cp ./neocompiler.io/docker-sock-express-compilers/docker-services/eco-metadata.json ./eco-metadata.json
+cp ../neocompiler.io/docker-sock-express-compilers/docker-services/eco-metadata.json ./eco-metadata.json
 git add .
 git commit -m "updating metadata"
 git push
