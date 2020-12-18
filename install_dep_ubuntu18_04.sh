802e9205c3ad232f82998f0bf5cebe4528e68ae7
Vitor <vncoelho@gmail.com>
2020-12-18 18:25:50 -0300

    minor changes on deps

--- install_dep_ubuntu18_04.sh
+++ install_dep_ubuntu18_04.sh
@@ -17,18 +17,19 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-
+   
 sudo apt-get update
 
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
 
 sudo chmod +x /usr/local/bin/docker-compose
 
-git clone https://github.com/NeoResearch/neocompiler-server.git
+#git clone https://github.com/NeoResearch/neocompiler-server.git
 
-cd neocompiler-server
+#cd neocompiler-server
 
 git clone https://github.com/NeoResearch/neocompiler-eco.git neocompiler.io
 
