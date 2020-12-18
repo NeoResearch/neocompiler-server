0a117867067affeed8f6034b1ce8c10675fd4f4e
Vitor <vncoelho@gmail.com>
2020-12-18 18:06:52 -0300

    Adding env var for updateAll

--- updateAllNeoCompiler.sh
+++ updateAllNeoCompiler.sh
@@ -1,10 +1,18 @@
+source ./.env
+
 touch /root/LIGOUSERVIDOR_ALL.txt
 rm /var/log/nginx/access.log
 
 ./updateMetadata.sh
 
 cd neocompiler.io
+if (($NEO2)); then
+	echo "CHECKOUT MASTER-2X";
+	git checkout master-2x
+fi
+
 git pull
+
 (cd docker-compose-eco-network; ./cleanVolumes.sh)
 
 ./build_everything.sh --no-web
