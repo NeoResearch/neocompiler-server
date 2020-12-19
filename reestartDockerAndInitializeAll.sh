bcb79efaa16863e1ad8929ff1bdc9088d705e05f
Vitor <vncoelho@gmail.com>
2020-12-19 00:32:10 -0300

    clean

--- reestartDockerAndInitializeAll.sh
+++ reestartDockerAndInitializeAll.sh
@@ -5,10 +5,10 @@ service docker restart
 
 echo "\n (RESTART) Docker prune network..."
 docker network prune -f
-
 echo "\n (RESTART) All dockers have been killed!"
-echo "\n (RESTART) NGIX Building, down and up dettached"
-docker-compose build && docker-compose down && docker-compose up -d
+
+echo "\n (RESTART) NGIX down and up dettached"
+(cd DockerNginxHub && docker-compose down && docker-compose up -d)
 
 echo "\n (RESTART) calling standard ./updateAllNeoCompiler script..."
 ./updateAllNeoCompiler.sh
