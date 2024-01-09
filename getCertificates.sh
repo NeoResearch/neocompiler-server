143f13ac758c134dbcd6611ffa3438a0919dc89c
Vitor Nazario Coelho <vncoelho@gmail.com>
2024-01-09 11:46:26 -0300

    adding rest

--- getCertificates.sh
+++ getCertificates.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-letsencrypt certonly --standalone --email igormachado@gmail.com -d neocompiler.io -d node1-neo3.neocompiler.io -d node2-neo3.neocompiler.io -d node3-neo3.neocompiler.io -d ecoservices-neo3.neocompiler.io -d compilers-neo3.neocompiler.io
+letsencrypt certonly --standalone --email igormachado@gmail.com -d neocompiler.io -d node1-neo3.neocompiler.io -d node2-neo3.neocompiler.io -d node3-neo3.neocompiler.io -d ecoservices-neo3.neocompiler.io -d compilers-neo3.neocompiler.io -d node3-neo3-rest.neocompiler.io 
